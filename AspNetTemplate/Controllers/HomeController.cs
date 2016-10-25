fd91ba1bdca73ec8646a73d73f9fb376d5144c97
Thiago Lunardi <thiago.lunardi@auctionata.com>
2016-10-25 18:39:05 +0200

    Exibindo ambiente

--- AspNetTemplate/Controllers/HomeController.cs
+++ AspNetTemplate/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,8 @@ namespace AspNetTemplate.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Ambiente = ConfigurationManager.AppSettings["Ambiente"];
+
             return View();
         }
 
